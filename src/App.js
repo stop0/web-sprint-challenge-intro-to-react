e6e0dc9c5176729d6baef82cbf310bb199cb51f8
MRSTOP0\Stop0 <dawodmohmmed@gmail.com>
2020-12-11 12:02:10 -0500

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Character from "./components/Character"
+import axios from "axios";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +11,28 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [starwars, setStarWars] = useState([])
+
+  useEffect(() =>{
+    axios
+      .get('https://swapi.py4e.com/api/people')
+      .then(resp =>{
+        console.log(resp.data.results)
+        setStarWars(resp.data.results)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  },[])
+
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1>WANTED</h1>
+      <h1>DEAD OR ALIVE</h1>
+      {starwars.map((name) =>
+      <Character starwars={name} className="Header" />
+      )}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,44 @@
 // Write your Character component here
+import React from "react";
+import Styled from 'styled-components'
+
+
+
+
+
+const Styledcard = Styled.div`
+ 
+    color : #ffe7b1;
+    padding:1px;
+    margin:30px;
+    background-color: #403e44;	
+
+    &:hover{
+        background-color :#fbc34a;
+        color:#403e44;
+    }
+`
+
+
+
+
+const Character = (props) => {
+    const {name , height, mass, gender,eye_color,skin_color,hair_color} = props.starwars
+    return(
+    <div>
+    <Styledcard>
+        <h1>{name}</h1>
+        <h3>Height : {height} cm</h3>
+        <h3>Weight : {mass} kilograms </h3>
+        <h3>Gender : {gender}</h3>
+        <h3>Skin Color : {skin_color}</h3>
+        <h3>Eye Color : {eye_color}</h3>
+        <h3>Hair Color : {hair_color}</h3>
+    </Styledcard>
+    </div>
+    )
+
+
+}
+
+export default Character
